83084f8d3bf9b9e58a75ccaf38d8b2354d693584
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-08-12 11:55:15 +0100

    testing lib

--- app/src/main/java/com/reza/custompopup/MainActivity.kt
+++ app/src/main/java/com/reza/custompopup/MainActivity.kt
@@ -3,18 +3,12 @@ package com.reza.custompopup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.reza.custompopup.ui.theme.CustomPopupTheme
 import com.reza.popup.BottomSheetPopupLayout
 
